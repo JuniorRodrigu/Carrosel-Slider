f3610d80921a45a9c878df829c12361ffafdbea4
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2022-06-04 13:30:14 -0300

    0.1

---
+++ components/produtor/index.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable @next/next/no-img-element */
+import style from '../../styles/produto.module.css';
+
+export default function Produtor  () {
+    return (
+        <div className={style.container}>
+            <div className={style.head}></div>
+
+            <div className={style.info}>
+                <div className={style.img}>
+                <img src="/tmp/aaaa.png" alt=""  />
+                </div>
+               
+            <div className={style.catNome}>transparente </div>
+<div className={style.nome}>Animal</div>
+<div className={style.price}>R$ 10</div></div>
+
+
+
+
+
+
+        </div>
+
+    );
+
+
+}
